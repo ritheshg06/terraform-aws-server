07195ccea3e2ebfa2fcfb006df3c12239871e3b6
Rithesh Girish Gowda <36320813+ritheshg06@users.noreply.github.com>
2021-02-25 15:33:49 -0800

    Update variables.tf

--- app-web-modules/variables.tf
+++ app-web-modules/variables.tf
@@ -2,6 +2,6 @@ variable "ami" {}
 variable "web_count" {}
 variable "subnet_id" {}
 variable "identity" {}
-#variable "vpc_security_group_ids" {
+variable "vpc_security_group_ids" {
   type = list
 }
